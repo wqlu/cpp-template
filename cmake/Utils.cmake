bdb3fdf3a386e4270cdd7b7044da559f2af7db62
wqlu <wq.lu@outlook.com>
2023-02-24 16:37:22 +0800

    init commit

---
+++ cmake/Utils.cmake
@@ -0,0 +1,23 @@
+
+#
+# Add a target for formatign the project using `clang-format` (i.e: cmake --build build --target
+# clang-format)
+#
+
+function(add_clang_format_target)
+  if(NOT ${PROJECT_NAME}_CLANG_FORMAT_BINARY)
+    find_program(${PROJECT_NAME}_CLANG_FORMAT_BINARY clang-format)
+  endif()
+
+  if(${PROJECT_NAME}_CLANG_FORMAT_BINARY)
+    if(${PROJECT_NAME}_BUILD_EXECUTABLE)
+      add_custom_target(clang-format
+                        COMMAND ${${PROJECT_NAME}_CLANG_FORMAT_BINARY}
+                        -i ${exe_sources} ${header}
+                        WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
+    elseif()
+    else()
+    endif()
+    message(STATUS "")
+  endif()
+endfunction()
